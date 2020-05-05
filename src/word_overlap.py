8807fd94688b06f7f4da136751ae5d85dc737339
John C. Merfeld <john.merfeld@gmail.com>
2020-05-05 14:56:33 -0400

    added clue decider

--- src/word_overlap.py
+++ src/word_overlap.py
@@ -6,6 +6,8 @@ import helpers
 import word_overlap
 word_overlap.best_connecting_word('blood', 'hood')
 
+word_overlap.best_clue(['slug', 'blood', 'hood'])
+
 """
 
 client = helpers.get_mongo_client(settings.MONGO_CLUSTER, settings.MONGO_DATABASE, settings.MONGO_USER, settings.MONGO_PASSWORD)
@@ -45,6 +47,34 @@ def best_connecting_word(base1, base2):
     best_score = 0
     best_match = {}
     for match in match_list:
+        if match['score'] > best_score:
+            best_match = match
+            best_score = match['score']
+
+    return best_match
+
+
+def best_clue(word_list):
+    """Get the best pairwise connecting word from a group of words"""
+    # create empty best_match_list
+    best_match_list = []
+    # for each pair of words in word_list:
+    for word1 in word_list:
+        for word2 in word_list:
+            if word1 != word2:
+                # get best_match and the overlap score
+                connection = best_connecting_word(word1, word2)
+
+                # add the base words, the match word, and the score to best_match_list
+                best_match_list.append({"word1": word1,
+                                        "word2": word2,
+                                        "connector": connection['word'],
+                                        "score": connection['score']})
+
+    # TODO: wrap in a helper function
+    best_score = 0
+    best_clue = {}
+    for match in best_match_list:
         if match['score'] > best_score:
             best_match = match
             best_score = match['score']
