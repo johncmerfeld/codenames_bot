a6420fd34e1c4a8f20f61478e70a90d1af4009fe
Kevin Merfeld <kevin.merfeld@zoro.com>
2020-05-08 15:48:45 -0500

    cleaning rules

--- src/game.py
+++ src/game.py
@@ -11,45 +11,64 @@ import settings
 
 
 class Game:
-    def __init__(self, client, database, collection, words):
+    def __init__(self, client, database, collection, words_by_team):
         """Class to play the game"""
         self.client = client
         self.database = database
         self.collection = collection
-        self.words = words
+        self.words_by_team = words_by_team
 
-    def get_words(self, key):
-        """Get words by key"""
+    def validate_team(self, team):
+        """Validate team input variable"""
 
-        if key not in ("red", "blue", "assassin"):
-            raise Exception(f"{key} not found. Expecting red, blue or assassin")
+        valid_teams = list(self.words_by_team.keys())
+        valid_teams_string = ", ".join(valid_teams)
+
+        if team not in valid_teams:
+            raise Exception(
+                f"{team} team not found. Expecting team {valid_teams_string}"
+            )
         else:
-            return self.words[key]
+            return True
+
+    def get_words(self, team):
+        """Get words by team"""
+
+        if self.validate_team(team):
+            return self.words_by_team[team]
 
     def add_words(self, word_or_words, team):
-        """Add words to game"""
+        """Add words to team"""
 
-        if not isinstance(word_or_words, list):
-            raise Exception(f"Expecting list but got {type(word_or_words).__name__}")
-        elif team not in ("red", "blue"):
-            raise Exception(f"{team} team not found. Expecting team red or blue")
-        else:
-            for word in word_or_words:
-                if word not in self.words[team]:
-                    self.words[team].append(word)
+        if self.validate_team(team):
+            if isinstance(word_or_words, str):
+                if word_or_words not in self.words_by_team[team]:
+                    self.words_by_team[team].append(word_or_words)
+                    print(f"{word_or_words} added to {team}")
+            elif isinstance(word_or_words, (list, tuple)):
+                for word in word_or_words:
+                    if word not in self.words_by_team[team]:
+                        self.words_by_team[team].append(word)
                     print(f"{word} added to {team}")
+            else:
+                raise Exception(
+                    f"Expecting str, list or tuple but got {type(word_or_words).__name__}"
+                )
 
     def remove_words(self, word_or_words):
         """Remove words from game"""
 
-        if not isinstance(word_or_words, list):
-            raise Exception(f"Expecting list but got {type(word_or_words).__name__}")
-        else:
-            for word in word_or_words:
-                for key, value in self.words.items():
-                    if word in value:
-                        value.remove(word)
-                        print(f"{word} removed from {key}")
+        if isinstance(word_or_words, str):
+            for team, team_words in self.words_by_team.items():
+                if word_or_words in team_words:
+                    team_words.remove(word_or_words)
+                    print(f"{word_or_words} removed from {team}")
+        elif isinstance(word_or_words, (list, tuple)):
+            for team, team_words in self.words_by_team.items():
+                for word in word_or_words:
+                    if word in team_words:
+                        team_words.remove(word)
+                        print(f"{word} removed from {team}")
 
     def get_best_connecting_words(self, words):
         """Get best connecting words. This is work in progress!"""
@@ -67,9 +86,7 @@ class Game:
     def give_clue(self, team):
         """Give clue for team read or blue"""
 
-        if team not in ("red", "blue"):
-            raise Exception(f"{team} team not found. Expecting team red or blue")
-        else:
+        if self.validate_team(team):
             words = self.get_words(team)
 
         return self.get_best_connecting_words(words)
@@ -82,17 +99,17 @@ client = helpers.get_mongo_client(
     settings.MONGO_PASSWORD,
 )
 
-words = {
+words_by_team = {
     "red": ["Iron", "Lemon", "Fair"],
     "blue": ["Drill", "Hollywood"],
     "assassin": "Foot",
 }
 
-game = Game(client, settings.MONGO_DATABASE, settings.collection, words)
+game = Game(client, settings.MONGO_DATABASE, settings.collection, words_by_team)
 
 print(game.get_words("red"))
-game.remove_words(["Iron"])
+game.remove_words(("Lemon", "Fair"))
 print(game.get_words("red"))
-game.add_words(["Spring"], "red")
+game.add_words("Spring", "red")
 print(game.get_words("red"))
 print(game.give_clue("red"))
