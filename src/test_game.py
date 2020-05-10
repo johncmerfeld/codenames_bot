e60041d9adafb257edf61fbbe3dba872acd982a5
Kevin Merfeld <kevin.merfeld@zoro.com>
2020-05-09 19:02:32 -0500

    adding tests

---
+++ src/test_game.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+import unittest
+
+from game import Game
+
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.client = "test_client"
+        self.database = "test_db"
+        self.collection = "test_collection"
+        self.words_by_team = {
+            "red": ["Fair", "Fish", "Dinosaur"],
+            "blue": ["Drill", "Hollywood", "Chocolate", "King"],
+            "assassin": ["Turkey"],
+        }
+        self.test_game = Game(
+            self.client, self.database, self.collection, self.words_by_team
+        )
+
+    def test_get_valid_teams(self):
+        self.assertEqual(self.test_game.get_valid_teams(), ["red", "blue", "assassin"])
+
+    def test_validate_team(self):
+        self.assertTrue(self.test_game.validate_team("red"))
+
+    def test_validate_team_negative(self):
+        self.assertRaises(Exception, self.test_game.validate_team, "yellow")
+
+
+if __name__ == "__main__":
+    unittest.main()
